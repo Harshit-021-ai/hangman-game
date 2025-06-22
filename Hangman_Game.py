cf420d380bcf7c57e4bd0b918bbc4b71fe3edd8d
Harshit Sharma <harshitsharma5810@gmail.com>
2025-06-22 19:26:46 +0530

    Update Hangman_Game.py

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -1,20 +1,40 @@
 import pygame
-import os
 
+# setup display
 pygame.init()
-Width = 800
-Height = 600
+Width, Height= 800, 600
 Window = pygame.display.set_mode((Width, Height))
 pygame.display.set_caption("Hangman Game By Harshit Sharma")
 
+# load images
+images = []
+for i in range(7):
+    image= pygame.image.load("hangman" + str(i) + ".png")
+    images.append(image)
+
+# game variables
+Hangman_status = 4
+
+# colors
+White = (225,225,225)
+
+# setup game loop
 FPS = 60
 clock = pygame.time.Clock()
 run = True
 
 while run:
     clock.tick(FPS)
+
+    Window.fill(White)
+    Window.blit(images[Hangman_status], (150,100))
+    pygame.display.update()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            print(pos)
 
 pygame.quit()
